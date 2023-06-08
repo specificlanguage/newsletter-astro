c9f386ca6496801940487f96d6de36439fc5e5f2
specificlanguage <specificlanguage@gmail.com>
2023-06-08 00:19:21 -0700

    feat: Cloudflare fixes
    - Adds cloudflare to astro
    - This may be followed up with several commits trial-erroring the cloudflare integration as normal

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  site: 'https://newsletter-astro.pages.dev',
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  output: "server",
+  adapter: cloudflare()
 });
