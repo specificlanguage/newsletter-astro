2d4daf77229c446acc581db150165f217b436334
specificlanguage <specificlanguage@gmail.com>
2023-06-08 00:47:33 -0700

    feat: fix RSS to properly add the pages that need to get added

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,18 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts.ts';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
 	const posts = await getCollection('blog');
+	// console.log(posts);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
-		site: context.site,
+		site: import.meta.env.SITE,
 		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
+			title: post.data.title,
+			link: post.slug,
+			pubDate: post.data.date
 		})),
 	});
 }
