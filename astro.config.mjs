b985f3d25a70482e008f5254e1fb3c8d7f62d753
specificlanguage <specificlanguage@gmail.com>
2023-06-08 00:58:55 -0700

    feat: accidentally added SSR, reverting back to SSG
    - Additionally removes spamming for headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,8 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://newsletter-astro.pages.dev',
   integrations: [mdx(), sitemap(), tailwind(), react()],
-  output: "server",
-  adapter: cloudflare()
 });
