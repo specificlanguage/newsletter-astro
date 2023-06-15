a5349894cce9114e7b9ad0841ba8c44fabb8bd33
specificlanguage <specificlanguage@gmail.com>
2023-06-15 17:41:40 -0400

    feat: Redo header & front page to be somewhat relevant - Adds pagination for the front page - Adds FontAwesome script back (but I don't like it at all) - Removes extras content collection, will add back in later

---
+++ src/frontPageHelpers.ts
@@ -0,0 +1,16 @@
+export function stripHTML(originalString: string)  {
+    return originalString.replace(/(<([^>]+)>)/gi, "")
+        .replace(/import.*;/g, "")
+        .replace(/(?:__|[*#])|\[(.*?)\]\(.*?\)/g, "$1")
+        .replace(/#/g, "")
+        .replace(/\*\*/g, "");
+}
+
+export function readingTime(str: string){
+    const num_words = str.split(" ").length;
+    if(num_words < 180) {
+        return "< 1 min read";
+    } else {
+        return (num_words / 180).toLocaleString(undefined, {maximumFractionDigits: 0}) + " min read";
+    }
+}
