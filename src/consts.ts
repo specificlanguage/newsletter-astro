cce4dddfecb68a7dcdbaa7bf560a6e7521ced28b
specificlanguage <specificlanguage@gmail.com>
2023-06-19 22:21:40 -0400

    feat: Final push towards the finish - Add June 19th newsletter & associated extra, and some archives - Edit icon link - Add image to not be default astro image - Modify parts of the Sidebar, ColoredBlock, and BlogPost layout to be a little tighter on the CSS - Remove the very stupid (Page 2) on the top of the pages list - Increase header depth to 4 and subsequently change sidebar to fix it

--- src/consts.ts
+++ src/consts.ts
@@ -4,4 +4,4 @@
 export const SITE_TITLE = 'CivWiki Monday Newsletter';
 export const SITE_DESCRIPTION = 'A semi-weekly newsletter for Civ servers supported by the CivWiki';
 
-export const MAX_HEADER_DEPTH = 3;
+export const MAX_HEADER_DEPTH = 4;
