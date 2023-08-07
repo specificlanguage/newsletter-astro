44dd3f699b6b98c64062dd96d19ca4112c569bc8
specificlanguage <specificlanguage@gmail.com>
2023-08-06 23:37:28 -0400

    feat: Links open in new tab
    Why is the default to open in the same tab aaaaaaaaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,22 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+import {inspectUrls} from "@jsdevtools/rehype-url-inspector";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://civwiki.news',
   integrations: [svelte(), mdx(), sitemap(), tailwind()],
+  markdown: {
+    rehypePlugins: [
+      [
+        inspectUrls, {
+        selectors: ["a[href]"],
+        inspectEach(url) {
+          url.node.properties.target = "_blank";
+        }
+      }
+      ]
+    ],
+  },
 });
