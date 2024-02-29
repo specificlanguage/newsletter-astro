3b1f7d91cc7bed87370a02c3f185c0c31b242c68
specificlanguage <specificlanguage@gmail.com>
2024-02-29 15:21:10 -0500

    feat(polls): Make poll look cleaner
    - Uses a lot of tailwind classes to make the checkboxes and polls look cleaner

--- src/components/PollQuestion.svelte
+++ src/components/PollQuestion.svelte
@@ -19,13 +19,14 @@
     </div>
     {#if question.type === QuestionType.MultipleChoice}
         {#each question.options as option}
-            <div class="h-fit my-1 ml-1">
+            <div class="h-fit m-1">
                 <label>
-                    <input type=radio bind:group={selected}
+                    <input class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600
+                                  dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-600 dark-border-gray-600"
+                           type=radio bind:group={selected}
                            name={question.questionID}
                            value={option.toLowerCase().replace(/ /g, "")}/>
-                    {option}
-
+                    <span class="ms-2">{option}</span>
                 </label>
             </div>
         {/each}
@@ -34,20 +35,26 @@
             <i>Select all that are applicable.</i>
         </div>
         {#each question.options as option}
-            <div class="h-fit my-1">
+            <div class="h-fit m-1">
                 <label>
-                    <input type=checkbox bind:group={selected}
+                    <input class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600
+                                  dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-600 dark-border-gray-600 rounded"
+                            type=checkbox bind:group={selected}
                            name={question.questionID}
                            value={option.toLowerCase().replace(/ /g, "")}/>
-                    {option}
+                    <span class="ms-2">{option}</span>
                 </label>
             </div>
         {/each}
     {:else if question.type === QuestionType.ShortResponse}
-        <input type="text" name={question.questionID} placeholder="Type here..."
-        class="rounded-md p-1 w-1/2 text-black text-lg border-4 border-transparent"/>
+        <div class="h-fit m-1">
+            <input type="text" name={question.questionID} placeholder="Type here..."
+            class="rounded-md ml-1 p-1 w-1/2 text-black text-lg border-4 border-transparent"/>
+        </div>
     {:else }
-        <textarea name={question.questionID} placeholder="Type some answer here..."
-        class="rounded-md p-1 w-3/4 text-black text-lg border-4 border-transparent"/>
+        <div class="h-fit m-1">
+            <textarea name={question.questionID} placeholder="Type some answer here..."
+            class="rounded-md p-1 w-3/4 text-black text-lg border-4 border-transparent"></textarea>
+        </div>
     {/if}
 </div>
