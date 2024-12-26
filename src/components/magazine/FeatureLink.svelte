6111998d31fa6139e1a662211815bd79dff586fd
specificlanguage <specificlanguage@gmail.com>
2024-12-26 11:13:42 +0800

    feat: Viewport sizes on Feature links
    Saw some errors when looking at the live version of feature links -- following Tailwind docs to make this simpler to implement.

--- src/components/magazine/FeatureLink.svelte
+++ src/components/magazine/FeatureLink.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <a class={
-        "rounded-3xl my-1 px-4 py-0.5 h-52 md:h-40 flex " +
+        "rounded-3xl my-1 px-4 py-0.5 h-full sm:h-48 flex items-center " +
         "dark:bg-neutral-700 bg-neutral-200 " +
         "border-2 border-neutral-500 " +
         "text-neutral-600 dark:text-neutral-400 " +
@@ -20,15 +20,19 @@
    href={"/" + feature.collection + "/" + feature.slug}>
 
     <!-- Body flexbox -->
-    <div class="flex space-x-2">
-        <div class={"w-1/2 md:w-2/3 my-auto "} >
-            <h3 class="m-0">{feature.data.title}</h3>
-            <p class="m-0 text-sm">{feature.data.author}</p>
-            <p class="my-2 text-base">{feature.data.description}</p>
+    <div class="flex flex-col">
+
+        <div class="flex space-x-2 py-2">
+            <div class={"w-full sm:w-2/3 my-auto"} >
+                <h3 class="m-0">{feature.data.title}</h3>
+                <p class="m-0 text-sm">{feature.data.author}</p>
+                <p class="my-2 text-base">{feature.data.description}</p>
+            </div>
+            <div class="hidden sm:flex sm:visible sm:w-1/3 px-0.5">
+                <img src={img_url} class="object-scale-down"/>
+            </div>
         </div>
-        <div class="w-1/2 md:w-1/3 p-4 flex">
-            <img src={img_url} class="w-full h-full"/>
-        </div>
+
     </div>
 
 </a>
