22f1d09429a590fe6727d60c24600fc8307f0b54
specificlanguage <specificlanguage@gmail.com>
2025-09-24 08:23:30 -0700

    September 24

--- src/components/magazine/BlockLink.svelte
+++ src/components/magazine/BlockLink.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <a class={
-        "rounded-3xl my-1 px-4 py-2 h-28 md:h-32 flex items-center overflow-hidden " +
+        "rounded-3xl my-1 px-4 py-2 h-32 md:h-32 flex items-center overflow-hidden " +
         "dark:bg-neutral-700 bg-neutral-200 " +
         "border-2 border-neutral-500 " +
         "text-neutral-600 dark:text-neutral-400 " +
@@ -17,8 +17,13 @@
    href={url}>
 
     <!-- Content section -->
-    <div class="w-full sm:w-2/3 pr-4 flex flex-col justify-center min-h-0">
-        <h3 class="m-0 truncate">{title} {#if icon_class !== undefined} <i class={icon_class}/> {/if} </h3>
+    <div class="w-full sm:w-2/3 pr-4 flex flex-col justify-center min-h-0 ">
+        <span class="flex flex-nowrap space-x-2">
+            <h4 class="truncate m-0 text-[18px] ">{title}</h4>
+            {#if icon_class !== undefined}
+                <p class="m-0"><i class={icon_class}/></p>
+            {/if}
+        </span>
         {#if author !== undefined}
             <p class="m-0 text-sm truncate">{author}</p>
         {/if}
