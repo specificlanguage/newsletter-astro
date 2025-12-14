f84e2331a53e1c0f62a4111ac19799e234b7f457
specificlanguage <specificlanguage@gmail.com>
2025-12-14 13:18:58 -0800

    Revert "Tailwind -> v4 upgrade"
    This reverts commit 2126ae1919b132a1d7099563504fcef12b665b1f.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import rehypeExternalLinks from "rehype-external-links";
 import cloudflare from '@astrojs/cloudflare';
@@ -9,7 +9,7 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://civwiki.news',
-  integrations: [svelte(), mdx(), sitemap(), tailwindcss()],
+  integrations: [svelte(), mdx(), sitemap(), tailwind()],
   adapter: cloudflare(),
   markdown: {
     rehypePlugins: [
